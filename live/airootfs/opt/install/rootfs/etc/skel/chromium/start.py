c48728e97cc3559b297051df71d742e54ccb378f
Victor B <victor.bonnelle@protonmail.com>
2021-12-30 00:24:46 +0100

    Improve python script

--- live/airootfs/opt/install/rootfs/etc/skel/chromium/start.py
+++ live/airootfs/opt/install/rootfs/etc/skel/chromium/start.py
@@ -3,7 +3,20 @@ import json
 import bottle
 
 options = webdriver.ChromeOptions()
+
+# Remove "restore pages" popups
+with open("~/.config/chromium/Default/Preferences", "r+") as f:
+    f.write(f.read().replace('"exited_cleanly":false', '"exited_cleanly":true').replace('"exit_type": "Crashed"', '"exit_type": "Normal"'))
+
+# Start in full screen
 options.add_argument("--app=https://www.google.com")
+
+# Use default profile
+options.add_argument("--profile-directory=Default")
+options.add_argument("user-data-dir=/home/user/.config/chromium")
+
+# Disable popups
+options.add_experimental_option("excludeSwitches", ["disable-popup-blocking"])
 driver = webdriver.Chrome(options=options)
 
 @bottle.route("/home")
